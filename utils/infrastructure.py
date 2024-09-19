8dcb6ecd8ac649a32a53724d7c55e196d3cb4773
Dennis Noll <dennis.noll@cern.ch>
2024-09-18 17:15:23 -0700

    [dev] infrastructure: can now use multiple clusters with SlurmFactory

--- utils/infrastructure.py
+++ utils/infrastructure.py
@@ -6,48 +6,53 @@ from dask import delayed, compute
 from functools import wraps
 
 
-def slurm_factory(
-    jobs=1,
-    queue="interactive",
-    cores=1,
-    memory="1GB",
-    walltime="00:30:00",
-    job_extra_directives=["-C cpu"],
-    *args,
-    **kwargs
-):
-    def slurm(f):
+class SlurmFactory:
+    def __init__(
+        self,
+        jobs=1,
+        queue="interactive",
+        cores=1,
+        memory="1GB",
+        walltime="00:30:00",
+        job_extra_directives=["-C cpu"],
+        *args,
+        **kwargs
+    ) -> None:
+        # Start SLURM Cluster
+        self.cluster = SLURMCluster(
+            queue=queue,
+            cores=cores,
+            memory=memory,
+            walltime=walltime,
+            job_extra_directives=job_extra_directives,
+            *args,
+            **kwargs
+        )
+
+        # Adjust the number of jobs as needed
+        self.cluster.scale(jobs=jobs)
+
+    def __del__(self):
+        self.cluster.close()
+
+    def slurm(self, f):
         @functools.wraps(f)
-        def decor(*func_args, **func_kwargs):
-            # Set up the SLURMCluster
-            cluster = SLURMCluster(
-                queue=queue,
-                cores=cores,
-                memory=memory,
-                walltime=walltime,
-                job_extra_directives=job_extra_directives,
-                *args,
-                **kwargs
-            )
-
-            # Scale the cluster to the desired number of workers
-            cluster.scale(jobs=jobs)  # Adjust the number of jobs as needed
-
+        def decor(*args, **kwargs):
             # Connect a Dask client to the cluster
-            client = Client(cluster)
+            client = Client(self.cluster)
 
             # Wrap the function call in a Dask delayed object
-            delayed_task = delayed(f)(*func_args, **func_kwargs)
+            delayed_task = delayed(f)(*args, **kwargs)
 
             # Execute the task using Dask
             result = compute(delayed_task)
 
             # Close the client and cluster when done
             client.close()
-            cluster.close()
 
+            # Return result of task
             return result
 
         return decor
 
-    return slurm
+slurm_factory = SlurmFactory()
