8c63241b48be7d6875388d541ae38bcf4f248ef3
Dennis Noll <dennis.noll@cern.ch>
2024-10-22 10:41:37 -0700

    [style] proc/test: unneeded code

--- processors/test.py
+++ processors/test.py
@@ -11,18 +11,6 @@ class Processor(processor.ProcessorABC):
     def process(self, events):
         pad = lambda x, target: ak.pad_none(x, target=target, clip=True)
         jets = pad(events.Jet, 2)
-        # jets = ak.zip(
-        #     {
-        #         "pt": jets.pt,
-        #         "eta": jets.eta,
-        #         "phi": jets.phi,
-        #         "mass": ak.zeros_like(jets.pt),
-        #         "charge": ak.zeros_like(jets.pt),
-        #     },
-        #     with_name="PtEtaPhiMCandidate",
-        #     behavior=candidate.behavior,
-        # )
-
         good = jets[:, 0].pt > 50
         good = ak.fill_none(good, False)
 
